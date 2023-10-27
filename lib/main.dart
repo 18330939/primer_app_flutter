3fab396127b2f22045fec9ac84eb03d63b5d9c58
18330939 <l18330939@hermosillo.tecnm.mx>
2023-10-27 12:16:35 -0700

    Centralizacion de los botones

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
           useMaterial3: true,
         ),
         home: const MyHomePage(),
@@ -43,16 +43,49 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context){
     var appState = context.watch<MyAppState>();
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Idea aleatoria"),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: (){
-              appState.getSiguiente();
-            }, 
-          child: Text("Siguiente")),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Idea aleatoria"),
+            SizedBox(height: 20,),
+            BigCard(idea: appState.current),
+            SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: (){
+                appState.getSiguiente();
+              }, 
+            child: Text("Siguiente")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  final WordPair idea;
+
+  const BigCard ({
+    super.key,
+    required this.idea
+  });
+
+  @override
+  Widget build(BuildContext context){
+    final tema = Theme.of(context);
+    final TextStyle = tema.textTheme.displayMedium!.copyWith(
+      color: tema.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: tema.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(idea.asLowerCase , 
+        style: TextStyle,
+        semanticsLabel: "${idea.first} ${idea.second}",
+        ),
       ),
     );
   }
