0d86e245b39fd22c3d08441d386da3f6909fac0f
18330939 <l18330939@hermosillo.tecnm.mx>
2023-10-30 12:47:04 -0700

    Agrego division para home y favoritos

--- lib/main.dart
+++ lib/main.dart
@@ -53,42 +53,33 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-    var appState = context.watch<MyAppState>();
-    var idea = appState.current;
-    IconData icon;
-    if(appState.favoritos.contains(idea)){
-      icon = Icons.favorite;
-    }else{
-      icon = Icons.favorite_outline;
-    }
+    
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Idea aleatoria"),
-            SizedBox(height: 20,),
-            BigCard(idea: appState.current),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: (){appState.toggleFavorito();},
-                  icon: Icon(icon), 
-                  label: Text("Me gusta")),
-                SizedBox(width: 10,),
-                ElevatedButton(
-                  onPressed: (){
-                    appState.getSiguiente();
-                  }, 
-                child: Text("Siguiente")),
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home), 
+                  label: Text("Inicio")),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite), 
+                  label: Text("Favoritos")),
               ],
-            ),
-          ],
-        ),
-      ),
+              selectedIndex: 0,
+              onDestinationSelected: (value){
+                print("Seleccion: $value");
+              },
+            )
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),)),
+        ],)
     );
   }
 }
@@ -118,5 +109,46 @@ class BigCard extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class GeneratorPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+    var idea = appState.current;
+    IconData icon;
+    if(appState.favoritos.contains(idea)){
+      icon = Icons.favorite;
+    }else{
+      icon = Icons.favorite_outline;
+    }
+
+    return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Idea aleatoria"),
+            SizedBox(height: 20,),
+            BigCard(idea: appState.current),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: (){appState.toggleFavorito();},
+                  icon: Icon(icon), 
+                  label: Text("Me gusta")),
+                SizedBox(width: 10,),
+                ElevatedButton(
+                  onPressed: (){
+                    appState.getSiguiente();
+                  }, 
+                child: Text("Siguiente")),
+              ],
+            ),
+          ],
+        ),
+      );
   }
 }
